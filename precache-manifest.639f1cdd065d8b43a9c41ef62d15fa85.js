2323097ed93acf50366d8667d46ad55c251e5b52
Mahdi <mahditahavorgar@gmail.com>
2020-12-08 21:07:50 +0330

    first

---
+++ precache-manifest.639f1cdd065d8b43a9c41ef62d15fa85.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d1fbc61bd32bca7ea60",
+    "url": "/static/js/main.fc3387ed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cdde8f268534d1bb61df",
+    "url": "/static/css/2.50da0a4f.chunk.css"
+  },
+  {
+    "revision": "cdde8f268534d1bb61df",
+    "url": "/static/js/2.2bc10ad4.chunk.js"
+  },
+  {
+    "revision": "4c58037b84e2b796d404d3cb2770a210",
+    "url": "/static/media/nothing.4c58037b.svg"
+  },
+  {
+    "revision": "fdbc4613a1ea54685aefe7e130011d3c",
+    "url": "/static/media/person.fdbc4613.svg"
+  },
+  {
+    "revision": "bcc287eaf67b81a8f6332e2dff80eb16",
+    "url": "/static/media/logo.bcc287ea.svg"
+  },
+  {
+    "revision": "0f4a7f7476ed84b3527c3f993de82a9c",
+    "url": "/static/media/tmdb.0f4a7f74.svg"
+  },
+  {
+    "revision": "27b65cb40d26f78354a4ac5abf87b2be",
+    "url": "/static/media/tmdbgreen.27b65cb4.svg"
+  },
+  {
+    "revision": "e0b5f43f549eaaae96de6476830468fa",
+    "url": "/static/media/empty.e0b5f43f.svg"
+  },
+  {
+    "revision": "cbc8724a992b15ec4f477b412ec14120",
+    "url": "/static/media/error.cbc8724a.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "539f82c4b1e1fa66350ac34cc1fa58f6",
+    "url": "/index.html"
+  }
+];
